7f232a2addae6f929c0b41df013151bdb66cac43
A01021066 <liuouis@hotmail.com>
2019-10-31 01:17:20 -0700

    wtf

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser'); 
 let app = express();
-let db = require('database/connection');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })) // middleware
 
